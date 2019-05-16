ba1430c4608309491269fe80a745025c6d5f4e1f
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2019-05-16 15:41:32 +0300

    Connecting To MongoDB With Mongoose (07)

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const {mongoUri} = require('./config/keys');
+
+mongoose.connect(mongoUri, {useNewUrlParser: true})
+    .then(() => console.log('successfully connected'))
+    .catch(e => console.log(e.message));
+
 
 const app = express();
+
 
 app.get('/', (req, res) => res.send('Hello world'));
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=> console.log(`server running on port: ${port}`));
+app.listen(port, () => console.log(`server running on port: ${port}`));
