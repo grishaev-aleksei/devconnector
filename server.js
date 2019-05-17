fc1ede27c0a57731f56a90d745c4ac9840845d40
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2019-05-17 11:19:01 +0300

    Route Files With Express Router (08)

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
 const {mongoUri} = require('./config/keys');
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
 
 mongoose.connect(mongoUri, {useNewUrlParser: true})
     .then(() => console.log('successfully connected'))
@@ -9,8 +13,13 @@ mongoose.connect(mongoUri, {useNewUrlParser: true})
 
 const app = express();
 
-
 app.get('/', (req, res) => res.send('Hello world'));
+
+app.use('/api/users', users);
+app.use('/api/posts', posts);
+app.use('/api/profile', profile);
+
+
 
 const port = process.env.PORT || 5000;
 
