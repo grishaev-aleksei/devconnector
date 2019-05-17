26e16a17666bb45c0f12c16ce68b6226cf21022f
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2019-05-17 16:20:06 +0300

    Wrapped with error catching (10)

--- routes/api/users.js
+++ routes/api/users.js
@@ -16,22 +16,26 @@ router.post('/register', async (req, res) => {
     if (user) {
         res.status(400).json({email: 'email already exists'})
     } else {
-        const avatar = gravatar.url(req.body.email,{
-            s: '200',
-            r: 'pg',
-            d: 'mm'
-        });
-        const newUser = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-            avatar,
-        });
+        try {
+            const avatar = gravatar.url(req.body.email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            });
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password,
+                avatar,
+            });
 
-        const salt = await bcrypt.genSalt(10);
-        newUser.password = await bcrypt.hash(newUser.password, salt);
-        const savedUser = await newUser.save();
-        res.json(savedUser);
+            const salt = await bcrypt.genSalt(10);
+            newUser.password = await bcrypt.hash(newUser.password, salt);
+            const savedUser = await newUser.save();
+            res.json(savedUser);
+        } catch (e) {
+            console.log(e.message)
+        }
     }
 });
 
